8096b4f9471fbe583f6fbb0782718a02d8ab2f9b
Jack <tiongyingkiat@gmail.com>
2017-01-08 18:35:07 +0800

    Initial Commit

---
+++ app/src/main/java/com/teamproblemsolver/fabsubmenu/MainActivity.java
@@ -0,0 +1,28 @@
+package com.teamproblemsolver.fabsubmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton mFAB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFAB = (FloatingActionButton) findViewById(R.id.menu_item);
+
+        mFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
